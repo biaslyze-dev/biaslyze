ec3e56611c600bb5cab6802186ed3ff605f1ca23
Tobias Sterbak <sterbak-it@outlook.com>
2023-04-14 11:22:21 +0200

    Improve plots and add to LimeScore

--- biaslyze/evaluation_results.py
+++ biaslyze/evaluation_results.py
@@ -101,8 +101,12 @@ class EvaluationResult:
     Based on keywords: {reasons_stats.most_common(20)}."""
         return representation_string
 
-    def dashboard(self):
-        """Dashboard"""
+    def dashboard(self, use_position=False):
+        """Return a bokeh dashboard.
+        
+        Args:
+            use_position: If True, use the normalized position for plotting.
+        """
         
         res_df = pd.DataFrame({
             "text": [sample.text for sample in self.biased_samples],
@@ -111,71 +115,90 @@ class EvaluationResult:
             "bias_keywords": [sample.bias_reasons for sample in self.biased_samples],
             "num_tokens": [sample.num_tokens for sample in self.biased_samples],
             "top_words": [sample.top_words for sample in self.biased_samples],
+            "score": [sample.score for sample in self.biased_samples],
         })
 
         res_df["bias_concepts_joined"] = res_df.bias_concepts.apply(lambda x: ", ".join(x))
         res_df["bias_keywords_joined"] = res_df.bias_keywords.apply(lambda x: ", ".join(x))
+        # the normalized position might be used instead of score
+        if use_position:
+            res_df["score"] = 1 - (res_df.keyword_position / res_df.num_tokens)
 
         def bkapp(doc):
             # update function for selection in histogram
-            def update(attr, old, new):
-                """Callback used for plot update when lasso selecting"""
-                subset = res_df[res_df.keyword_position.isin(new)].sort_values(by=['keyword_position'])
-                source.data = subset
-
-            source = ColumnDataSource(data=dict())
-
-            # define the text columns
+            # define the table columns
             columns = [
                 TableColumn(field="text", title="text", width=800),
                 TableColumn(field="bias_concepts_joined", title="bias_concepts", width=100),
-                TableColumn(field="keyword_position", title="keyword_position", width=50),
                 TableColumn(field="bias_keywords_joined", title="bias_keywords", width=100),
-                TableColumn(field="num_tokens", title="num_tokens", width=50)
+                TableColumn(field="score", title="LimeScore", width=50),
+                TableColumn(field="keyword_position", title="Keyword Position", width=50),
+                TableColumn(field="num_tokens", title="Num unique tokens", width=50),
             ]
             source = ColumnDataSource(res_df)
-
             data_table = DataTable(
-                source=source, columns=columns, width=1100
+                source=source, columns=columns, width=1200
             )
-            source.data = res_df
-
+            
             # define histogram part
-        
-            ## prepare the data
-            plot_df = (res_df[["text", "keyword_position", "bias_concepts_joined"]]
-            .groupby(["keyword_position", "bias_concepts_joined"])
-            .count()
-            .unstack()
-            .fillna(0)
-            )
-            plot_df.columns = plot_df.columns.droplevel(0)
-            plot_df = (
-                pd.DataFrame({"keyword_position": np.arange(0, plot_df.index.max()+1, 1)})
-                .join(plot_df, how="left")
-                .fillna(0)
-                .set_index("keyword_position")
-            )
-            biases = plot_df.columns.tolist()
+            n_bins = 30 if use_position else 50
+            xmin, xmax = 0 if use_position else -1, 1
+            concepts_present = res_df.bias_concepts_joined.unique().tolist()
+            bar_alpha = 0.6
+            total_range = np.linspace(xmin, xmax, n_bins)
             
-            plot_dict = plot_df.to_dict("list")
-            plot_dict["position"] = [str(x) for x in plot_df.index.tolist()]
-
-            p = figure(x_range=plot_dict["position"], height=500, width=800, title="Bias keyword positions", tools="pan,wheel_zoom,xbox_select,reset", active_drag="xbox_select")
-
-            stacked_bars = p.vbar_stack(biases, x='position', width=0.9, color = Spectral[11][:len(biases)], source=plot_dict, legend_label=biases)
+            # update function for selection in histogram
+            def update(attr, old, new):
+                """Callback used for plot update when lasso selecting"""
+                if new:
+                    new_min_score, new_max_score = total_range[new[0]], total_range[new[-1]]
+                    subset = (
+                        res_df[
+                            (res_df.score > new_min_score) & (res_df.score < new_max_score)
+                        ]
+                        .sort_values(by=['score'], ascending=False)
+                    )
+                else:
+                    subset = res_df.copy()
+                source.data = subset
             
-            p.y_range.start = 0
-            p.x_range.range_padding = 0.1
-            p.xgrid.grid_line_color = None
-            p.axis.minor_tick_line_color = None
-            p.outline_line_color = None
-            p.legend.location = "top_right"
-            p.legend.orientation = "horizontal"
-                
-            # selection handler
-            for bar in stacked_bars:
+            # plot
+            p = figure(
+                x_range=(xmin, xmax),
+                height=600,
+                width=1200,
+                title="LimeScore of present bias concepts",
+                tools="pan,wheel_zoom,xbox_select,reset", active_drag="xbox_select"
+            )
+            
+            for bias_concept, color in zip(concepts_present, Spectral[11]):
+                # calculate the histogram
+                _hist, _ = np.histogram(
+                    res_df[res_df.bias_concepts_joined == bias_concept].score,
+                    bins=n_bins,
+                    range=(xmin, xmax)
+                )
+                # create the bar plot for this histogram
+                bar = p.vbar(
+                    x=total_range,
+                    top=_hist,
+                    width=(xmax - xmin)/n_bins,
+                    color=color,
+                    alpha=bar_alpha,
+                    legend_label=bias_concept,
+                )
+                # selection handler
                 bar.data_source.selected.on_change("indices", update)
+
+            # configure the appearance and the legend
+            p.y_range.start = 0
+            p.xgrid.grid_line_color = None
+            p.legend.orientation = "vertical"
+            p.legend.location = "top_right"
+            p.legend.click_policy="hide"
+            p.legend.title = 'Protected Concept'
+            p.legend.title_text_font_style = "bold"
+            p.legend.title_text_font_size = "16px"
             
             # put everything together
             doc.add_root(column(p, data_table))
