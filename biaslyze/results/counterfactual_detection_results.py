2f40a17d142f9b36887398d2e7bbe802d1008d35
Tobias Sterbak <sterbak-it@outlook.com>
2023-08-03 11:25:43 +0200

    Remove bubble plot

--- biaslyze/results/counterfactual_detection_results.py
+++ biaslyze/results/counterfactual_detection_results.py
@@ -249,121 +249,121 @@ class CounterfactualDetectionResult:
 
         return dashboard
 
-    def visualize_counterfactual_score_by_sample(self, concept: str):
-        """Visualize the counterfactual scores for each sample for a given concept."""
-        import yaml
-        from bokeh.layouts import column
-        from bokeh.models import ColumnDataSource, HoverTool, Slider
-        from bokeh.palettes import Category10_10
-        from bokeh.plotting import figure
-        from bokeh.themes import Theme
-        from sentence_transformers import SentenceTransformer
-        from umap import UMAP
-
-        dataf = self._get_result_by_concept(concept=concept)
-        samples = self._get_counterfactual_samples_by_concept(concept=concept)
-
-        # get the original samples
-        original_samples = [
-            sample for sample in samples if (sample.keyword == sample.orig_keyword)
-        ]
-
-        # get text representations in 2d
-        docs = [sample.text for sample in original_samples]
-        sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
-        embeddings = sentence_model.encode(docs, show_progress_bar=True)
-
-        # Train BERTopic
-        reduced_embeddings = UMAP(
-            n_neighbors=10, n_components=2, min_dist=0.0, metric="cosine"
-        ).fit_transform(embeddings)
-
-        # build the visualization
-        def bkapp(doc):
-            hover_tool = HoverTool(
-                tooltips=[
-                    ("Text", "@text"),
-                    ("Keywords", "@keywords"),
-                    ("Counterfactual score", "@counterfactual_sample_score"),
-                ]
-            )
-            p = figure(
-                width=1200,
-                height=800,
-                tools=["pan", "wheel_zoom", "box_zoom", "reset", hover_tool],
-            )
-
-            bias_intensity = np.array(dataf.mean(axis=1))
-
-            # configure
-            df = pd.DataFrame(
-                dict(
-                    text=[sample.text for sample in original_samples],
-                    keywords=[sample.keyword for sample in original_samples],
-                    x=reduced_embeddings[:, 0],
-                    y=reduced_embeddings[:, 1],
-                    color=[
-                        Category10_10[idx] for idx in (bias_intensity <= 0).astype(int)
-                    ],
-                    bias_intensity=500 * np.abs(bias_intensity),
-                    counterfactual_sample_score=np.round(bias_intensity, 4),
-                )
-            )
-            source = ColumnDataSource(data=df)
-
-            # add a circle renderer with a size, color, and alpha
-            p.scatter(
-                "x",
-                "y",
-                source=source,
-                color="color",
-                size="bias_intensity",
-                alpha=0.3,
-            )
-
-            # p.legend.location = "top_left"
-            # p.legend.click_policy="hide"
-
-            # slider
-            threshold = Slider(
-                title="threshold",
-                value=0.0,
-                start=0.0,
-                end=df.bias_intensity.max(),
-                step=0.01,
-                width=750,
-            )
-
-            def update_data(attrname, old, new):
-                # Get the current slider values
-                t = threshold.value
-                new_df = df.copy()
-                new_df["bias_intensity"] = new_df.bias_intensity.apply(
-                    lambda x: x if x >= t else 0.0
-                )
-                source.data = new_df
-
-            threshold.on_change("value", update_data)
-
-            doc.add_root(column(threshold, p, width=800))
-
-            # show the results
-            doc.theme = Theme(
-                json=yaml.load(
-                    """
-                attrs:
-                    figure:
-                        background_fill_color: "#DDDDDD"
-                        outline_line_color: white
-                        toolbar_location: above
-                        height: 800
-                        width: 1200
-                    Grid:
-                        grid_line_dash: [6, 4]
-                        grid_line_color: white
-            """,
-                    Loader=yaml.FullLoader,
-                )
-            )
-
-        return bkapp
+#    def visualize_counterfactual_score_by_sample(self, concept: str):
+#        """Visualize the counterfactual scores for each sample for a given concept."""
+#        import yaml
+#        from bokeh.layouts import column
+#        from bokeh.models import ColumnDataSource, HoverTool, Slider
+#        from bokeh.palettes import Category10_10
+#        from bokeh.plotting import figure
+#        from bokeh.themes import Theme
+#        from sentence_transformers import SentenceTransformer
+#        from umap import UMAP
+#
+#        dataf = self._get_result_by_concept(concept=concept)
+#        samples = self._get_counterfactual_samples_by_concept(concept=concept)
+#
+#        # get the original samples
+#        original_samples = [
+#            sample for sample in samples if (sample.keyword == sample.orig_keyword)
+#        ]
+#
+#        # get text representations in 2d
+#        docs = [sample.text for sample in original_samples]
+#        sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+#        embeddings = sentence_model.encode(docs, show_progress_bar=True)
+#
+#        # Train BERTopic
+#        reduced_embeddings = UMAP(
+#            n_neighbors=10, n_components=2, min_dist=0.0, metric="cosine"
+#        ).fit_transform(embeddings)
+#
+#        # build the visualization
+#        def bkapp(doc):
+#            hover_tool = HoverTool(
+#                tooltips=[
+#                    ("Text", "@text"),
+#                    ("Keywords", "@keywords"),
+#                    ("Counterfactual score", "@counterfactual_sample_score"),
+#                ]
+#            )
+#            p = figure(
+#                width=1200,
+#                height=800,
+#                tools=["pan", "wheel_zoom", "box_zoom", "reset", hover_tool],
+#            )
+#
+#            bias_intensity = np.array(dataf.mean(axis=1))
+#
+#            # configure
+#            df = pd.DataFrame(
+#                dict(
+#                    text=[sample.text for sample in original_samples],
+#                    keywords=[sample.keyword for sample in original_samples],
+#                    x=reduced_embeddings[:, 0],
+#                    y=reduced_embeddings[:, 1],
+#                    color=[
+#                        Category10_10[idx] for idx in (bias_intensity <= 0).astype(int)
+#                    ],
+#                    bias_intensity=500 * np.abs(bias_intensity),
+#                    counterfactual_sample_score=np.round(bias_intensity, 4),
+#                )
+#            )
+#            source = ColumnDataSource(data=df)
+#
+#            # add a circle renderer with a size, color, and alpha
+#            p.scatter(
+#                "x",
+#                "y",
+#                source=source,
+#                color="color",
+#                size="bias_intensity",
+#                alpha=0.3,
+#            )
+#
+#            # p.legend.location = "top_left"
+#            # p.legend.click_policy="hide"
+#
+#            # slider
+#            threshold = Slider(
+#                title="threshold",
+#                value=0.0,
+#                start=0.0,
+#                end=df.bias_intensity.max(),
+#                step=0.01,
+#                width=750,
+#            )
+#
+#            def update_data(attrname, old, new):
+#                # Get the current slider values
+#                t = threshold.value
+#                new_df = df.copy()
+#                new_df["bias_intensity"] = new_df.bias_intensity.apply(
+#                    lambda x: x if x >= t else 0.0
+#                )
+#                source.data = new_df
+#
+#            threshold.on_change("value", update_data)
+#
+#            doc.add_root(column(threshold, p, width=800))
+#
+#            # show the results
+#            doc.theme = Theme(
+#                json=yaml.load(
+#                    """
+#                attrs:
+#                    figure:
+#                        background_fill_color: "#DDDDDD"
+#                        outline_line_color: white
+#                        toolbar_location: above
+#                        height: 800
+#                        width: 1200
+#                    Grid:
+#                        grid_line_dash: [6, 4]
+#                        grid_line_color: white
+#            """,
+#                    Loader=yaml.FullLoader,
+#                )
+#            )
+#
+#        return bkapp
