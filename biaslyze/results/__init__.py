8953288dfadbd9c9118cfe3d11dd57280383b23e
Tobias Sterbak <sterbak-it@outlook.com>
2023-08-24 16:14:50 +0200

    Update website

--- biaslyze/results/__init__.py
+++ biaslyze/results/__init__.py
@@ -1,3 +1,4 @@
+"""This module contains the results of the biaslyze package."""
 from .counterfactual_detection_results import (  # noqa
     CounterfactualConceptResult,
     CounterfactualDetectionResult,
